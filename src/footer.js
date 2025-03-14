0e26953e679043e7056dbb30409f99d8a49408af
Narongwas <darkwar008@gmail.com>
2025-03-14 16:29:41 +0700

    add button join now and some word

--- src/footer.js
+++ src/footer.js
@@ -4,10 +4,10 @@ function Footer(){
     return(
         <section className="footer">
             <div id="col">
-                <h2>Contact Us</h2>
-                <p>Email: [email]</p>
-                <p>Phone: [phone]</p>
-                <p>Address: [address]</p>
+                <div id="innercol">
+                    <h4 id="footerh4">Don't hesitate – join us right now!</h4>
+                    <button className="footerButton">JOIN NOW</button>
+                </div>
             </div>
         </section>
     )
