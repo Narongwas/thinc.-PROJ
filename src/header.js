12ec5c467fa3e5ebe2f81275fbb0f72bb405a90a
Narongwas <darkwar008@gmail.com>
2025-03-15 00:38:09 +0700

    Edit ส่วนที่เหลือให้ตรงตาม figma

--- src/header.js
+++ src/header.js
@@ -9,8 +9,8 @@ function Header(){
     return (<header className="App-header">
         <div className="left-Header">
               <a className="path" id="Home"><b>Name</b></a>
-              <a className="path" onClick={onClick}><img src={logo} alt="toMainMenu"></img></a>
-              <a className="path">เกี่ยวกับเรา</a>
+              <a className="path" onClick={onClick} id="image"><img src={logo} alt="toMainMenu"></img></a>
+              <a className="path" id="AboutMe">เกี่ยวกับเรา</a>
           </div>
         <div className="Right-header">
               <button id="Login" className="headerButton">Log In</button>
