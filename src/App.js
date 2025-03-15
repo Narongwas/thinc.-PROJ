7161f8cf006f46ed35e10246b67ef9ceab3cbd07
Kannawich <kannawich@gmail.com>
2025-03-16 02:15:28 +0700

    Add Homepage that collect all the post (No css yet) and change server.js path

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import MainPage from './Pages/MainPage.js'; 
 import PostPage from './Pages/PostPage.js';
+import Home from './Pages/Home.js';
 function App() {
   return (
     <Router>
@@ -9,10 +10,10 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} /> 
           <Route path="/post" element={<PostPage />} /> 
+          <Route path="/Home" element={<Home />} />
         </Routes>
       </div>
     </Router>
   );
 }
-
 export default App;
