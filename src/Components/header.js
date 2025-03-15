3ea97ef35500114b5e3d798d55a971687882f890
Narongwas <darkwar008@gmail.com>
2025-03-15 21:22:06 +0700

    แยก lefthead มาตั้งเป็นฟังก์ชันแยกและทำให้ logo home สามารถกลับไปหน้า main menu ได้

--- src/Components/header.js
+++ src/Components/header.js
@@ -7,7 +7,7 @@ function Header(){
         window.location.href = "index.html";
     }
     return (<header className="App-header">
-        <Leftheadonanypage onClick={onClick}/>
+        <Leftheadonanypage />
         <div className="Right-header">
               <button id="Login" className="headerButton">Log In</button>
               <button id="signup" className="headerButton">Join Now</button>
--- src/Components/leftHeader.js
+++ src/Components/leftHeader.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from '../Assets/home.png';
-function Leftheadonanypage({onClick}){
+import { Link } from 'react-router-dom';
+function Leftheadonanypage(){
     return (
         <div className="left-Header">
-            <a className="path" id="Home" onClick={onClick}><b>Name</b></a>
+            <a className="path" id="Home"><b>Name</b></a>
+            <Link to="/">
             <a className="path" id="image"><img src={logo} alt="toMainMenu"></img></a>
+            </Link>
             <a className="path" id="AboutMe">เกี่ยวกับเรา</a>
         </div>
     )
